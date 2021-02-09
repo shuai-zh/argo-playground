0fee2a4c69e24f16990b09589be471655ab7244c
Shuai Zhang <szhang1@godaddy.com>
2021-02-08 23:43:52 -0800

    expose argo workflow server via NodePort; update docker tags and readme

--- README.md
+++ README.md
@@ -7,19 +7,7 @@
 
 ## Setup
 
-### TLDR;
-
 Run `./start.sh`
-
-### Manual
-
-1. Install `kind` and `argo` CLI with `asdf`: `asdf plugin add kind; asdf plugin add argo; asdf install`
-2. Create a kind cluster with `kind create cluster --name argo-playground --config kind-config.yaml`
-3. Create a `ClusterRoleBinding` with `kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=default:default`
-4. Add the Argo Helm repo with `helm repo add argo https://argoproj.github.io/argo-helm`
-5. Install `Argo Workflows` Helm chart with `helm upgrade --install --atomic argo-workflows argo/argo -n argo -f helm/charts/argo-workflows/values.yaml --create-namespace`
-6. Install `Argo Events` Helm chart with `helm upgrade --install --atomic argo-events argo/argo-events -n argo -f helm/charts/argo-events/values.yaml`
-7. Change the NodePort host port to `30000` (defined in `./kind-config.yaml`) with `kubectl patch svc argo-workflows-server -n argo --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value": 30000}]'`
 
 ## Get Started
 
--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -e
 
+# install asdf plugins and cli tools
 asdf plugin add kind || echo 'skipping'
 asdf plugin add argo || echo 'skipping'
 asdf install
 kind create cluster --name argo-playground --config kind-config.yaml || echo 'skipping'
-kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=default:default || echo 'skipping'
+
+# install helm charts
 helm repo add argo https://argoproj.github.io/argo-helm
-helm upgrade --install --atomic argo-workflows argo/argo -n argo -f helm/charts/argo-workflows/values.yaml --create-namespace
-helm upgrade --install --atomic argo-events argo/argo-events -n argo -f helm/charts/argo-events/values.yaml --create-namespace
-kubectl patch svc argo-workflows-server -n argo --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value": 30000}]'
+helm upgrade --install --atomic argo-workflows argo/argo -n argo-workflows -f helm/charts/argo-workflows/values.yaml --create-namespace
+helm upgrade --install --atomic argo-events argo/argo-events -n argo-events -f helm/charts/argo-events/values.yaml --create-namespace
+helm upgrade --install --atomic argo-cd argo/argo-cd -n argo-cd -f helm/charts/argo-cd/values.yaml --create-namespace
+
+# update k8s resources
+kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=default:default || echo 'skipping'
+kubectl patch svc argo-workflows-server -n argo-workflows --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value": 30000}]'
+kubectl patch svc argo-cd-argocd-server -n argo-cd --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value": 30001}, {"op": "replace", "path": "/spec/ports/1/nodePort", "value": 30002}]'
+
 echo "Done!🎉"
